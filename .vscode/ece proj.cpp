288bae25413c19058fe1f182dfc5d881d5a4bc3d
azizo3197-coder <azizo3197@gmail.com>
2026-04-10 14:51:17 +0200

    update code

--- .vscode/ece proj.cpp
+++ .vscode/ece proj.cpp
@@ -4,6 +4,7 @@ int main() {
     int a, b;
     cout << "Enter two numbers: ";
     cout<< "Enter the first number: ";
+    cout<< "Enter the second number: ";
     cin >> a >> b;
 
     int sum = a + b;
